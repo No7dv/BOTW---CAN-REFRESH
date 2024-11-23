e946cdb62878b94a05f12edf179ffbba0272eb78
No7_dv <jeux-app-eve_7@outlook.fr>
2024-11-23 16:23:33 +0100

    Add files via upload

---
+++ scripts/lenis.js
@@ -0,0 +1,46 @@
+const lenis = new Lenis({
+  lerp: 0.1,
+  smoothWheel: true,
+  smoothTouch: true,
+  normalizeWheel: true,
+  direction: 'vertical', // Commence avec un défilement vertical
+});
+
+function animate(time) {
+  lenis.raf(time);
+  requestAnimationFrame(animate);
+}
+requestAnimationFrame(animate);
+
+document.querySelector('a[href="#section__carousel"]').addEventListener('click', (e) => {
+  e.preventDefault(); // Empêche le comportement par défaut
+
+  const target = document.querySelector('#section__carousel'); // Section cible
+
+  const offset = 105; // Ajuste cette valeur pour décaler vers le haut
+  const targetPosition = target.getBoundingClientRect().top + window.scrollY - offset;
+
+  lenis.scrollTo(targetPosition); // Scrolle avec un décalage de 30px vers le haut
+});
+
+document.querySelector('a[href="#link__navbar"]').addEventListener('click', (e) => {
+  e.preventDefault(); // Empêche le comportement par défaut
+
+  const target = document.querySelector('#link__navbar'); // Section cible
+
+  const offset = 2; // Ajuste cette valeur pour décaler vers le haut
+  const targetPosition = target.getBoundingClientRect().top + window.scrollY - offset;
+
+  lenis.scrollTo(targetPosition); // Scrolle avec un décalage de 30px vers le haut
+});
+
+document.querySelector('a[href="#section__about"]').addEventListener('click', (e) => {
+  e.preventDefault(); // Empêche le comportement par défaut
+
+  const target = document.querySelector('#section__about'); // Section cible
+
+  const offset = -250; // Ajuste cette valeur pour décaler vers le haut
+  const targetPosition = target.getBoundingClientRect().top + window.scrollY - offset;
+
+  lenis.scrollTo(targetPosition); // Scrolle avec un décalage de 30px vers le haut
+});
---
+++ scripts/swiper.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const swiper = new Swiper('.swiper-container', {
+        speed: 800,
+        effect: 'slide',
+        slidesPerView: 1,
+        spaceBetween: 10,
+        loop: true,
+        freeModeMomentumRatio: 0.2, // Réduit la sensibilité (par défaut, c'est 1)
+        freeModeMomentumVelocityRatio: 0.2, // Pour ralentir davantage
+            mousewheel: {
+                forceToAxis: true, // Permet de prioriser un axe (horizontal ici)
+                releaseOnEdges: true, // Evite les blocages en fin de carousel
+                speed: 800,
+            },
+            navigation: {
+                nextEl: '.swiper-button-next',
+                prevEl: '.swiper-button-prev',
+            },
+            pagination: {
+                el: '.swiper-pagination',
+                clickable: true,
+            },
+        });
+});
